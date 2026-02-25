d607577516b4b26c79fe95b5ed95f6b91f396403
eakeem <eakeem0@gmail.com>
2026-02-25 14:40:31 -0500

    added js to img and product description

--- java/javascript.js
+++ java/javascript.js
@@ -2,3 +2,21 @@ function emailnews(){
     document.getElementById("email").innerHTML
     alert("You have subscribe to our newsletter");
 }
+var Mainimage= document.getElementById("Mainimage");
+var smallimg= document.getElementsByClassName("smallimg");
+smallimg[0].onclick=function(){
+  Mainimage.src= smallimg[0].src;  
+}
+smallimg[1].onclick=function(){
+  Mainimage.src= smallimg[1].src;  
+}
+smallimg[2].onclick=function(){
+  Mainimage.src= smallimg[2].src;  
+}
+smallimg[3].onclick=function(){
+  Mainimage.src= smallimg[3].src;  
+}
+smallimg[4].onclick=function(){
+  Mainimage.src= smallimg[4].src;  
+}
+
