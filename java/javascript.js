618d67b1766f3371ccfa6f3e3f1a5484226337f7
eakeem <eakeem0@gmail.com>
2026-04-01 21:54:10 -0500

    fixing nav bar

--- java/javascript.js
+++ java/javascript.js
@@ -73,6 +73,15 @@ window.onclick = function(event) {
 
 
 
+function MyFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
 
 
 
