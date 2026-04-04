e3256984524fb2d4dd7987b1becd60c086d0226a
eakeem <eakeem0@gmail.com>
2026-04-03 23:02:18 -0500

    media q

--- java/javascript.js
+++ java/javascript.js
@@ -73,13 +73,14 @@ window.onclick = function(event) {
 
 
 
-function MyFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+function showSidebar(){
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'flex'
+}
+
+function hideSidebar(){
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'none'
 }
 
 
