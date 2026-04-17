4b90931b247ef72e26fbad94ab448ef1a56eca49
eakeem <eakeem0@gmail.com>
2026-04-17 11:50:48 -0500

    where i am stuck

--- java/javascript.js
+++ java/javascript.js
@@ -86,7 +86,11 @@ function hideSidebar(){
 
 
 
-
+const cartIcon = document.querySelector("#cart-icon");
+const cart = document.querySelector(".cart");
+const cartClose = document.querySelector("#cart-close");
+cartIcon.addEventListener("click", () => cart.classList.add("active"));
+cartClose.addEventListener("click", () => cart.classList.remove("active"));
 
 
 
